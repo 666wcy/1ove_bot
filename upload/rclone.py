e39864fb9d5e60696bd41d37f9a77c552a63db95
666wcy <769020367@qq.com>
2021-02-02 17:01:28 +0800

    Update rclone.py

--- upload/rclone.py
+++ upload/rclone.py
@@ -2,14 +2,18 @@
 # -*- coding: UTF-8 -*-
 from subprocess import Popen, PIPE
 import os
-
 import json
+import time
+import datetime
+import pytz
 os.chdir(os.path.dirname(__file__))
 with open('config.json', 'r', encoding='utf-8') as f:
     conf = json.loads(f.read())
     f.close()
 
-
+tz = pytz.timezone('Asia/Shanghai') #东八区
+#t = datetime.datetime.fromtimestamp(int(time.time()),tz).strftime('%Y年%m月%d日 %H:%M:%S')
+upload_data = datetime.datetime.fromtimestamp(int(time.time()),tz).strftime('%Y年%m月%d日')
 
 def start_upload_move(dir,folder,Remote,Upload):
     print(folder)
@@ -25,14 +29,14 @@ def start_upload_move(dir,folder,Remote,Upload):
     print(folder)
     if folder!="":
         print("上传判断，文件夹")
-        command=f"rclone --config /config/rclone/rclone.conf move  \"{dir}\"  \"{Remote}:{Upload}/{folder}\"  --stats=5s --stats-one-line --stats-log-level NOTICE --log-file=\"rclone_upload.log\" "
+        command=f"rclone --config /config/rclone/rclone.conf move  \"{dir}\"  \"{Remote}:{Upload}/{upload_data}/{folder}\"  --stats=20s --stats-one-line --stats-log-level NOTICE "
         print(command)
         resultsCommond = Popen(command,stdout=PIPE,
                                stderr=PIPE,stdin=PIPE,shell=True)
         data = resultsCommond.stdout.read()
         return f"{Remote}:{Upload}/{folder}"
     else:
-        command=f"rclone --config /config/rclone/rclone.conf move  \"{dir}\"  \"{Remote}:{Upload}\"  --stats=5s --stats-one-line --stats-log-level NOTICE --log-file=\"rclone_upload.log\" "
+        command=f"rclone --config /config/rclone/rclone.conf move  \"{dir}\"  \"{Remote}:{Upload}/{upload_data}\"  --stats=20s --stats-one-line --stats-log-level NOTICE  "
         resultsCommond = Popen(command,stdout=PIPE,
                                stderr=PIPE,stdin=PIPE,shell=True)
         data = resultsCommond.stdout.read()
@@ -52,14 +56,14 @@ def start_upload(dir,folder,Remote,Upload):
     print(folder)
     if folder!="":
         print("上传判断，文件夹")
-        command=f"rclone --config /config/rclone/rclone.conf copy  \"{dir}\"  \"{Remote}:{Upload}/{folder}\"  --stats=5s --stats-one-line --stats-log-level NOTICE --log-file=\"rclone_upload.log\" "
+        command=f"rclone --config /config/rclone/rclone.conf copy  \"{dir}\"  \"{Remote}:{Upload}/{upload_data}/{folder}\"  --stats=5s --stats-one-line --stats-log-level NOTICE "
         print(command)
         resultsCommond = Popen(command,stdout=PIPE,
                                stderr=PIPE,stdin=PIPE,shell=True)
         data = resultsCommond.stdout.read()
         return f"{Remote}:{Upload}/{folder}"
     else:
-        command=f"rclone --config /config/rclone/rclone.conf copy  \"{dir}\"  \"{Remote}:{Upload}\"  --stats=5s --stats-one-line --stats-log-level NOTICE --log-file=\"rclone_upload.log\" "
+        command=f"rclone --config /config/rclone/rclone.conf copy  \"{dir}\"  \"{Remote}:{Upload}/{upload_data}\"  --stats=5s --stats-one-line --stats-log-level NOTICE"
         print(command)
         resultsCommond = Popen(command,stdout=PIPE,
                                stderr=PIPE,stdin=PIPE,shell=True)
