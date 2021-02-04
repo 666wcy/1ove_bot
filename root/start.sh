6b89836bfd04fc22f69e187b52b9d290f6be11f2
666wcy <769020367@qq.com>
2021-02-04 10:47:02 +0800

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir  /root/.config/rclone/
-echo "${rclone}" >> /root/.config/rclone/rclone.conf
+#mkdir  /root/.config/rclone/
+#echo "${rclone}" >> /root/.config/rclone/rclone.conf
 ulimit -m 314400
 ulimit -v 314400
 /usr/bin/qbittorrent-nox --webui-port=$PORT
