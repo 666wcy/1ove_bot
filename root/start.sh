2ffda1078dcc219b2857815013a2547ee1e5eb29
666wcy <769020367@qq.com>
2021-02-04 10:41:43 +0800

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-chmod 0777 upload.sh
+
 mkdir  /root/.config/rclone/
 echo "${rclone}" >> /root/.config/rclone/rclone.conf
+ulimit -m 314400
+ulimit -v 314400
 /usr/bin/qbittorrent-nox --webui-port=$PORT
 #nohup yes "" | qbittorrent-nox --webui-port=$PORT --profile=/config &
