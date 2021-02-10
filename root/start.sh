4c7d19744aa6a1904c05629accaf0b34abc1da42
666wcy <769020367@qq.com>
2021-02-10 12:21:52 +0800

    new

--- root/start.sh
+++ root/start.sh
@@ -1,5 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+
+# 检查config配置文件，并创建
+if [ ! -e "/config/qBittorrent/config/qBittorrent.conf" ] ;  then 
+mkdir -p /config/qBittorrent/config/
+cp /usr/local/qbittorrent/defaults/qBittorrent.conf  /config/qBittorrent/config/qBittorrent.conf
+fi
+
+# permissions
+chown -R /config \
+    /downloads
+
+
 chmod 0777 /upload/ -R
+
+nohup yes "" | qbittorrent-nox --webui-port=$PORT  --profile=/config  &
 python3 /upload/config.py
-nohup yes "" | qbittorrent-nox --webui-port=$PORT --profile=/config &
-python3 /upload/bot.py
+python3 /upload/dingshi.py
--- upload/config.py
+++ upload/config.py
@@ -1,14 +1,33 @@
 # -*- coding: UTF-8 -*-
 import json
 import os
+import qbittorrentapi
+import sys
 os.chdir(os.path.dirname(__file__))
 QB_port=os.environ.get('PORT')
 Telegram_bot_api=os.environ.get('Telegram_bot_api')
 Telegram_user_id=os.environ.get('Telegram_user_id')
 Rule=os.environ.get('Rule')
+Username=os.environ.get('Username')
+Password=os.environ.get('Password')
 Rule=Rule.split("\n")
 
+
 rclone=os.environ.get('rclone')
+
+def change():
+    qbt_client = qbittorrentapi.Client(host=f'localhost:{QB_port}', username='admin', password='adminadmin')
+# this are all the same attributes that are available as named in the
+#  endpoints or the more pythonic names in Client (with or without 'app_' prepended)
+
+    prefs = qbt_client.app.preferences
+    prefs['web_ui_password'] = Password
+    qbt_client.app.preferences = prefs
+    prefs = qbt_client.app.preferences
+    prefs['web_ui_username'] = Username
+    qbt_client.app.preferences = prefs
+    print(f"修改账号：{Username}\n修改密码为:{Password}\n")
+    sys.stdout.flush()
 
 def mkdir(path):
     # 引入模块
@@ -36,14 +55,15 @@ def mkdir(path):
 print(f"Telegram_bot_api:{Telegram_bot_api}\n"
       f"Telegram_user_id:{Telegram_user_id}\n"
       f"QB_port:{QB_port}\n"
-      f"Rule:{Rule}"
-      f"rclone:{rclone}")
+      f"Rule:{Rule}\n"
+      f"rclone:{rclone}\n")
 
 mkdir("/config/rclone")
 with open("/config/rclone/rclone.conf", "w") as f:
     f.write(rclone)
     f.close()
 
+change()
 with open("/upload/config.json", "r",encoding='utf-8') as jsonFile:
     data = json.load(jsonFile)
     jsonFile.close()
@@ -51,6 +71,8 @@ with open("/upload/config.json", "r",encoding='utf-8') as jsonFile:
 data["QB_port"] = QB_port
 data["Telegram_bot_api"] = Telegram_bot_api
 data["Telegram_user_id"] = Telegram_user_id
+data["QB_username"]=Username
+data["QB_password"]=Password
 
 new_rule=[]
 for a in  Rule:
